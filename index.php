4e87c743aa98feb58fbf28e2c14e35ca572e0310
govinda707 <govindarana707@gmail.com>
2024-10-19 10:13:52 +0545

    products and category section added

--- index.php
+++ index.php
@@ -8,12 +8,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
      <!-- font awsome link -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
  <!--navbar  -->
  <div class="container-fluid p-0">
     <!-- first child -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <ul class="navbar-nav me-auto">
+  <li class="nav-item">
+  <a class="nav-link" href="#">Welcome Guest</a>
+    <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </ul>
+ </nav>
+
+ <!-- second navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-success">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Kisan Connect</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,10 +39,7 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Products</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
-        </li>
+        
         <li class="nav-item">
           <a class="nav-link" href="#">contact</a>
         </li>
@@ -43,23 +53,14 @@
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-light" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
 
-<!-- second nav bar -->
- <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-  <ul class="navbar-nav me-auto">
-  <li class="nav-item">
-  <a class="nav-link" href="#">Welcome Guest</a>
-    <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Register</a>
-          </ul>
- </nav>
+<!-- end of second navbar -->
+
  <!-- Third Child  -->
   <div class="bg-light">
     <h3 class="text-center">Hidden Store</h3>
@@ -72,45 +73,100 @@
     <div class="col-md-10">
       <!-- Products  -->
        <div class="row">
-       <div class="col-md-4">
+       <div class="col-md-4 mb-2">
        <div class="card">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="./images/cat-1.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+    <a href="#" class="btn btn-success">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  </div>
 </div>
        </div>
-       <div class="col-md-4">
+       <div class="col-md-4 mb-2">
        <div class="card">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="./images/cat-2.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+    <a href="#" class="btn btn-success">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  </div>
 </div>
        </div>
-       <div class="col-md-4">
+       <div class="col-md-4 mb-2">
        <div class="card">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="./images/cat-3.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-success">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+       <div class="card">
+  <img src="./images/cat-3.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+       <div class="card">
+  <img src="./images/cat-4.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
   </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+       <div class="card">
+  <img src="./images/cat-3.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  </div>
 </div>
 </div>
        </div>
     </div>
-    <div class="col-md-2">
-      <!-- Side bar -->
-       <h1>Side bar</h1>
+
+
+
+    <!-- Side bar -->
+    <div class="col-md-2 bg-secondary p-0">
+       <ul class="navbar-nav me-auto text-center">
+        <li class="nav-item  bg-info">
+            <a href="#" class="nav-link text-light bg-success">category</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Vegetables</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Fruits</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Dairy Products</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Domestic Animals </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-light">Meat Products</a>
+        </li>
+       </ul>
     </div>
    </div>
+
+
 <!-- footer section -->
- <div class="bg-info p-3 text-center">
+ <div class="bg-success p-3 text-center">
 <p>All right reserved Kisan Connect -2024</p>
  </div>
  </div>
