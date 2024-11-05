5904b5168dcf5e5904be5daaba2d175663b29ba2
govinda707 <govindarana707@gmail.com>
2024-11-05 11:41:32 +0545

    insert category interface designed

--- admin_panel/insert_category.php
+++ admin_panel/insert_category.php
@@ -3,8 +3,9 @@
   <span class="input-group-text bg-info" id="addon-wrapping"><i class="fa-solid fa-receipt"></i></span>
   <input type="text" class="form-control" namew="cat_title"placeholder="Insert Category" aria-label="Username" aria-describedby="addon-wrapping">
 </div>
-<div class="input-group w-10 mb-2">
-  <input type="insert" class="form-control bg-info" name="cat_insert" value="Insert Category">
+<div class="input-group w-10 mb-2 m-auto">
+  <!-- <input type="submit" class="form-control bg-info" name="cat_insert" value="Insert Category"> -->
+<button class="bg-info p-3 my-3 border-0">Insert Category</button>
 </div>
     
 </form>
